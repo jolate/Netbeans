874ad91028a0375575a027411ce909c75037617c
Juan Olate <jolate@gmail.com>
2011-11-17 11:41:59 -0300

    Added println.

---
+++ NetBeansProjects/Lab2/src/lab2/Lab2.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab2;
+
+/**
+ *
+ * @author jota
+ */
+public class Lab2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       System.out.println("nada");
+    }
+}
